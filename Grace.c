4500ae3ffb8865705d42fae7763724734bbb12bb
ulefebvr <ulefebvr@student.42.fr>
2017-03-24 13:11:14 +0100

    [GRACE] Exercice 2 done

---
+++ Grace.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+//grace
+#define D1
+#define D2
+#define D3 int main(){FILE*f=fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%1$c//grace%1$c#define D1%1$c#define D2%1$c#define D3 int main(){FILE*f=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);char*a=%2$c%3$s%2$c;fprintf(f,a,10,34,a);}%1$cD3%1$c";fprintf(f,a,10,34,a);}
+D3
