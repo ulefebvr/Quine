ad92192a1f5ad272400ac9186edc893a5b0c0633
ulefebvr <ulefebvr@student.42.fr>
2017-03-24 18:06:59 +0100

    [COLLEEN] Fix for correction

--- Colleen.c
+++ Colleen.c
@@ -2,4 +2,4 @@
 /*
 	c1
 */
-int add(int a, int b){return (a + b);}int main(void){/*c2*/char*a="#include <stdio.h>%1$c/*%1$c%2$cc1%1$c*/%1$cint add(int a, int b){return (a + b);}int main(void){/*c2*/char*a=%3$c%4$s%3$c;printf(a,10,9,34,a);}%1$c";printf(a,10,9,34,a);}
+int add(int a, int b){return (a + b);}int main(void){/*c2*/char*a="#include <stdio.h>%1$c/*%1$c%2$cc1%1$c*/%1$cint add(int a, int b){return (a + b);}int main(void){/*c2*/char*a=%3$c%4$s%3$c;printf(a,add(5,5),9,34,a);}%1$c";printf(a,add(5,5),9,34,a);}
