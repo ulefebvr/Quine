4baf4624abbd954d9034c36deef6adb86391c309
ulefebvr <ulefebvr@student.42.fr>
2017-03-24 19:21:38 +0100

    [BONUS] Colleen in python

---
+++ bonus/Colleen.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# c1
+def add(a,b):
+	return(a+b)
+if __name__=='__main__':
+	# c2
+	a='#!/usr/bin/env python3{nl}# c1{nl}def add(a,b):{nl}{tb}return(a+b){nl}if __name__=={quote}__main__{quote}:{nl}{tb}# c2{nl}{tb}a={quote}{str}{quote};print(a.format(str=a,nl=chr(add(5,5)),tb=chr(9),quote=chr(39)))';print(a.format(str=a,nl=chr(add(5,5)),tb=chr(9),quote=chr(39)))
