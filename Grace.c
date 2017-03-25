87fe9462e5ab6db7074124cc5aad0bcd88c231d8
ulefebvr <ulefebvr@student.42.fr>
2017-03-25 16:43:45 +0100

    [GRACE] Grace fied

--- Grace.c
+++ Grace.c
@@ -4,5 +4,5 @@
 */
 #define D1
 #define D2
-#define D3 int main(){FILE*f=fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%1$c/*%1$c%4$cc1%1$c*/%1$c%1$c#define D1%1$c#define D2%1$c#define D3 int main(){FILE*f=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);char*a=%2$c%3$s%2$c;fprintf(f,a,10,34,a);}%1$cD3%1$c";fprintf(f,a,10,34,a,9);}
+#define D3 int main(){FILE*f=fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%1$c/*%1$c%4$cc1%1$c*/%1$c#define D1%1$c#define D2%1$c#define D3 int main(){FILE*f=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);char*a=%2$c%3$s%2$c;fprintf(f,a,10,34,a,9);}%1$cD3%1$c";fprintf(f,a,10,34,a,9);}
 D3
